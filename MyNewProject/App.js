d8e28faf20bc3ca262cec6931c5e324aa8389ac1
pnllaryea <pnllaryea@st.ug.edu.gh>
2024-05-27 11:33:03 -0400

    initial commit

---
+++ MyNewProject/App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        My name is <Text style={styles.boldText}>Patrick</Text></Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ff9882',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+});
